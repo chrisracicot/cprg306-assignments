e8255476edcd23cc8b5cb363a9f0ab5719117073
Christin R <106452220+chrisracicot@users.noreply.github.com>
2024-09-18 18:06:26 -0600

    Update page.js and week-2 page.js, student-info.js, added License
    Added a link to week-2 folder of repo, within the main page.js file
    Added page.js and student-info.js to week-2 folder
    Added MIT License

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Christin Racicot</h1>
+      <Link href="https://github.com/chrisracicot/cprg306-assignments/">
+        https://github.com/chrisracicot/cprg306-assignments/
+      </Link>
+    </main>
+  );
+}
