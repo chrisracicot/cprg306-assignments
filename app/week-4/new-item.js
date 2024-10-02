8479fe02c4e2f80813d2040a07389e9453a4da6b
Christin R <106452220+chrisracicot@users.noreply.github.com>
2024-10-02 12:35:45 -0600

    Modified buttons and colours

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,23 +1,45 @@
 "use client";
-//indicates tells Next.js to run this code on the client side
+// indicates tells Next.js to run this code on the client side
 
 import { useState } from "react";
 
 export default function NewItem() {
   const [quantity, setCount] = useState(1);
+  //const [isClicked, setIsClicked] = useState(false); // New state to track button click
 
-  const increment = () => quantity < 20 && setCount(quantity + 1);
-
-  const decrement = () => quantity > 1 && setCount(quantity - 1);
+  const increment = () => {
+    if (quantity < 20) {
+      setCount(quantity + 1);
+      //setIsClicked(true); // Set the clicked state to true
+    }
+  };
 
   return (
-    <div>
-      <p>Amount: {quantity}</p>
-
-      <button disabled={increment > 20} onClick={increment}>
+    <div className="bg-orange-400 rounded-2xl p-4 border-double border-orange-300 border-4">
+      <p className="text-black text-center pb-3 font-bold text-2xl">
+        Amount: {quantity}
+      </p>{" "}
+      {/* Centered text */}
+      <button
+        className={`w-40 p-2 pl-4 m-3 rounded-2xl text-black ${
+          quantity >= 20
+            ? "bg-gray-400 border-solid border-2 border-gray-500"
+            : "bg-green-400 border-solid border-2 border-green-500" // Default
+        }`}
+        disabled={quantity >= 20}
+        onClick={increment}
+      >
         Increment
       </button>
-      <button disabled={decrement <= 1} onClick={decrement}>
+      <button
+        className={`w-40 p-2 pl-4 m-3 rounded-2xl text-black ${
+          quantity <= 1
+            ? "bg-gray-400 border-solid border-2 border-gray-500"
+            : "bg-green-400 border-solid border-2 border-green-500"
+        }`}
+        disabled={quantity <= 1}
+        onClick={() => setCount(quantity - 1)}
+      >
         Decrement
       </button>
     </div>
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item.js";
 
 export default function Page() {
   return (
-    <main className="">
+    <main className="flex justify-center w-full">
       <NewItem />
     </main>
   );
