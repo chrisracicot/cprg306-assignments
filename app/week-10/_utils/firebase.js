5534dcdce43edbbacef1a0b780a73e448e397432
Christin R <106452220+chrisracicot@users.noreply.github.com>
2024-11-20 06:18:48 -0700

    Update firebase.js

--- app/week-10/_utils/firebase.js
+++ app/week-10/_utils/firebase.js
@@ -1,3 +1,5 @@
+import { getFirestore } from "firebase/firestore";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,3 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const db = getFirestore(app);
